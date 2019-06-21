240fa89b60a05d7511a9baeb087a162eaa81cc0a
sethut <sethut1224@naver.com>
2019-06-22 01:12:20 +0900

    5G test

--- listener/src/listener.cpp
+++ listener/src/listener.cpp
@@ -1,27 +1,22 @@
 #include <ros/ros.h>
-#include <std_msgs/Int32MultiArray.h>
+#include "talker/echomsg.h"
 class echosub {
     private:
         ros::NodeHandle nh;
         ros::Subscriber subscriber;
         ros::Publisher publisher;
-        std_msgs::Int32MultiArray msg;
+        talker::echomsg msg;
     public:
         echosub(){
-            subscriber=nh.subscribe("pub_to_sub",1,&echosub::Callback,this);
-            publisher=nh.advertise<std_msgs::Int32MultiArray>("sub_to_pub",1);
+            subscriber=nh.subscribe("pub_to_sub",100,&echosub::Callback,this);
+            publisher=nh.advertise<talker::echomsg>("sub_to_pub",100);
         }
-        void Callback(const std_msgs::Int32MultiArray::ConstPtr& ptr){
+        void Callback(const talker::echomsgConstPtr& ptr){
             msg.data=ptr->data;
-            msg.layout.data_offset=ptr->layout.data_offset;
+            msg.delay=ptr->delay;
             ROS_INFO("msg arrived!");
             publisher.publish(msg);
         }
-        /*void pub()
-        {
-            publisher.publish(msg);
-            loop_rate.sleep();
-        }*/
 };
 
 int main(int argc, char* argv[]){
@@ -29,4 +24,3 @@ int main(int argc, char* argv[]){
     echosub eh;
     ros::spin();
 }
-
--- talker/src/talker.cpp
+++ talker/src/talker.cpp
@@ -1,8 +1,6 @@
     #include <ros/ros.h>
-    #include <std_msgs/Int32MultiArray.h>
+    #include "talker/echomsg.h"
     #include <vector>
-    #include <queue>
-    #include <ctime>
     class echopub{
         private://for check taken time 
             double start;
@@ -11,37 +9,35 @@
             int count;
             std::vector<double>time_arr; //save taken time
             std::vector<double>time_start;
-            //std::queue<double>time_start;
         private:
             ros::NodeHandle nh;
             ros::Publisher publisher;
             ros::Subscriber subscriber;
-            std_msgs::Int32MultiArray msg;
+            talker::echomsg msg;
         public:
             echopub(int size){
                 for(int i=0; i<size/4; i++)
                     msg.data.push_back(1); //resize msg for various data size
-                msg.layout.data_offset=0;
-                publisher=nh.advertise<std_msgs::Int32MultiArray>("pub_to_sub",1);
-                subscriber=nh.subscribe("sub_to_pub",1,&echopub::echocallback,this);
+                msg.count=0;
+                publisher=nh.advertise<talker::echomsg>("pub_to_sub",100);
+                subscriber=nh.subscribe("sub_to_pub",100,&echopub::echocallback,this);
             }
             void pub(){ 
-                //ROS_INFO("msg size : %ldbyte  pub_to_sub : %d",
-                //4*msg.data.size()+8,count,msg.pub_to_sub);;
-                msg.layout.data_offset++;
-                time_start.push_back(ros::Time::now().toSec());
+                msg.count++;
+                msg.delay=ros::Time::now().toSec();
                 publisher.publish(msg);
             }
-            void echocallback(const std_msgs::Int32MultiArrayConstPtr& ptr){         
+            void echocallback(const talker::echomsgConstPtr& ptr){         
                 end=ros::Time::now().toSec();
-                time=end-time_start[ptr->layout.data_offset];
+                time=end-ptr->delay;
                 time_arr.push_back(time);
-                ROS_INFO("CB msg size : %ldbyte  time : %f count : %ld",4*msg.data.size(),time, time_arr.size());
+                ROS_INFO("CB msg size : %ldbyte  time : %f count : %ld",4*msg.data.size(),time,time_arr.size());
             }
             void time_result(){
                 double sum,avg_time=0;
                 double max,min=time_arr.front();
                 int count=time_arr.size();
+
                 for(std::vector<double>::iterator it=time_arr.begin(); it!=time_arr.end(); ++it){
                     if(*it>max) max=*it;
                     else if(*it<min) min=*it;
@@ -54,11 +50,10 @@
                 ROS_INFO("min time : %f",min);
             }
     };
-
-
     int main(int argc, char* argv[]){
-        if(argc!=3){
+        if(argc!=3&&argc!=5){
             ROS_ERROR("input msg_size test_time");
+            exit(argc);
         }
         int msg_size=atoi(argv[1]);
         int test_time=atoi(argv[2]);   
@@ -68,12 +63,11 @@
 
         for(int i=0; i<test_time; i++)    {
             epub.pub();
-            ros::spinOnce();        
-            loop_rate.sleep();
+            ros::spinOnce();
+            loop_rate.sleep();        
         }
         epub.time_result();
         return 0;
     }
-
 
 
